c85ec4925b3093eaee030f4041f64e4679a61357
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-04-06 07:36:17 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -1,17 +1,1 @@
-(function() {
-    var s = document.createElement('script');
-    s.src = 'https://shebudriftaiter.net/tag.min.js';
-    s.setAttribute('data-zone', '9173610');
-
-    function appendScript() {
-        var target = document.body || document.documentElement;
-        target.appendChild(s);
-    }
-
-    // Check if document.body is ready
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', appendScript);
-    } else {
-        appendScript();
-    }
-})();
+console.log("theme design by dk technozone")
