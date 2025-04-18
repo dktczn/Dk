d2b3b9d97ac24816f42681c5c1ea26b10cc97231
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-04-18 07:29:31 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -1,43 +1,21 @@
-(function () {
+(function() {
     var currentHour = new Date().getHours();
 
-    // Load custom script between 9 PM and 4 AM
+    // Check if current time is between 6 PM (18) and 4 AM (4)
     if (currentHour >= 21 || currentHour < 4) {
-        var customScript = document.createElement('script');
-        customScript.src = 'https://shebudriftaiter.net/tag.min.js';
-        customScript.setAttribute('data-zone', '9173610');
+        var s = document.createElement('script');
+        s.src = 'https://shebudriftaiter.net/tag.min.js';
+        s.setAttribute('data-zone', '9173610');
 
-        function appendCustomScript() {
+        function appendScript() {
             var target = document.body || document.documentElement;
-            target.appendChild(customScript);
+            target.appendChild(s);
         }
 
         if (document.readyState === 'loading') {
-            document.addEventListener('DOMContentLoaded', appendCustomScript);
+            document.addEventListener('DOMContentLoaded', appendScript);
         } else {
-            appendCustomScript();
+            appendScript();
         }
     }
-
-    // Lazy load Google Analytics on scroll
-    var lazyAnalytics = false;
-    window.addEventListener("scroll", function () {
-        if ((document.documentElement.scrollTop !== 0 || document.body.scrollTop !== 0) && lazyAnalytics === false) {
-            var gaScript = document.createElement("script");
-            gaScript.type = "text/javascript";
-            gaScript.async = true;
-            gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
-
-            gaScript.onload = function () {
-                window.dataLayer = window.dataLayer || [];
-                function gtag() { dataLayer.push(arguments); }
-                gtag('js', new Date());
-                gtag('config', 'G-2VYRMPXK0F');
-            };
-
-            var firstScript = document.getElementsByTagName("script")[0];
-            firstScript.parentNode.insertBefore(gaScript, firstScript);
-            lazyAnalytics = true;
-        }
-    }, true);
 })();
