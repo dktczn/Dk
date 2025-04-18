c64bb08d8ab985de3bc93868f10e2138850e209c
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-04-18 07:25:09 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -1,48 +1,43 @@
-(function() {
+(function () {
     var currentHour = new Date().getHours();
 
-    // Check if current time is between 6 PM (18) and 4 AM (4)
+    // Load custom script between 9 PM and 4 AM
     if (currentHour >= 21 || currentHour < 4) {
-        var s = document.createElement('script');
-        s.src = 'https://shebudriftaiter.net/tag.min.js';
-        s.setAttribute('data-zone', '9173610');
+        var customScript = document.createElement('script');
+        customScript.src = 'https://shebudriftaiter.net/tag.min.js';
+        customScript.setAttribute('data-zone', '9173610');
 
-        function appendScript() {
+        function appendCustomScript() {
             var target = document.body || document.documentElement;
-            target.appendChild(s);
+            target.appendChild(customScript);
         }
 
         if (document.readyState === 'loading') {
-            document.addEventListener('DOMContentLoaded', appendScript);
+            document.addEventListener('DOMContentLoaded', appendCustomScript);
         } else {
-            appendScript();
+            appendCustomScript();
         }
     }
+
+    // Lazy load Google Analytics on scroll
+    var lazyAnalytics = false;
+    window.addEventListener("scroll", function () {
+        if ((document.documentElement.scrollTop !== 0 || document.body.scrollTop !== 0) && lazyAnalytics === false) {
+            var gaScript = document.createElement("script");
+            gaScript.type = "text/javascript";
+            gaScript.async = true;
+            gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
+
+            gaScript.onload = function () {
+                window.dataLayer = window.dataLayer || [];
+                function gtag() { dataLayer.push(arguments); }
+                gtag('js', new Date());
+                gtag('config', 'G-2VYRMPXK0F');
+            };
+
+            var firstScript = document.getElementsByTagName("script")[0];
+            firstScript.parentNode.insertBefore(gaScript, firstScript);
+            lazyAnalytics = true;
+        }
+    }, true);
 })();
-
-// Google Analytics lazy load on scroll
-var lazyanalisis = false;
-
-window.addEventListener("scroll", function () {
-    if (
-        (document.documentElement.scrollTop !== 0 && lazyanalisis === false) ||
-        (document.body.scrollTop !== 0 && lazyanalisis === false)
-    ) {
-        (function () {
-            var e = document.createElement("script");
-            e.type = "text/javascript";
-            e.async = true;
-            e.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
-            var a = document.getElementsByTagName("script")[0];
-            a.parentNode.insertBefore(e, a);
-        })();
-        lazyanalisis = true;
-    }
-}, true);
-
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-2VYRMPXK0F');
