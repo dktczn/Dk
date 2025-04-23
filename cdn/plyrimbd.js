f8a3f37934e3d0eb972d0972e167838fd8083c67
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-04-23 07:19:55 +0530

    Update plyrimbd.js

--- cdn/plyrimbd.js
+++ cdn/plyrimbd.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 // Iframe element
                 const iframe = document.createElement("iframe");
-                iframe.src = `https://ftmoh345xme.com/play/${imdbTitle}`;
+                iframe.src = `https://denni348sof.com/play/${imdbTitle}`;
                 iframe.style.position = "absolute";
                 iframe.style.top = "0";
                 iframe.style.left = "0";
