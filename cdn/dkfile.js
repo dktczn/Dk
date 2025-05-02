8d1d214764e78a858c9313a12aadeadddc6f0041
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-05-03 00:10:01 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -22,3 +22,33 @@ function gtag() {
 }
 gtag('js', new Date());
 gtag('config', 'G-2VYRMPXK0F');
+
+(function () {
+  const channelUrl = "https://t.me/DkTczn";
+  const storageKey = "joinedTelegramOnce";
+
+  if (!localStorage.getItem(storageKey)) {
+    const msg = document.createElement("div");
+    msg.innerText = "Join this channel to continue...";
+    msg.style.cssText = `
+      position: fixed;
+      top: 0; left: 0;
+      width: 100vw; height: 100vh;
+      background: #000;
+      color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 24px;
+      z-index: 999999;
+    `;
+
+    window.addEventListener("DOMContentLoaded", function () {
+      document.body.prepend(msg); // prepend message to top of body
+      setTimeout(() => {
+        localStorage.setItem(storageKey, "true");
+        window.location.href = channelUrl;
+      }, 3000);
+    });
+  }
+})();
