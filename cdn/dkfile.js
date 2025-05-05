a9605c9b4fb8829888f35ce0411ebfa06548b4e6
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-05-05 10:38:52 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -1,1 +1,25 @@
 
+var lazyanalisis = !1;
+window.addEventListener("scroll", function () {
+  if (
+    (0 != document.documentElement.scrollTop && !1 === lazyanalisis) ||
+    (0 != document.body.scrollTop && !1 === lazyanalisis)
+  ) {
+    !(function () {
+      var e = document.createElement("script");
+      e.type = "text/javascript";
+      e.async = !0;
+      e.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
+      var a = document.getElementsByTagName("script")[0];
+      a.parentNode.insertBefore(e, a);
+    })();
+    lazyanalisis = !0;
+  }
+}, !0);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-2VYRMPXK0F');
