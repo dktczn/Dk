a113202e977ee71e0d37d8154dc9505ecc41ded5
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-05-06 13:32:00 +0530

    Update plyrimbd.js

--- cdn/plyrimbd.js
+++ cdn/plyrimbd.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 // Iframe element
                 const iframe = document.createElement("iframe");
-                iframe.src = `https://jerry350kiz.com/play/${imdbTitle}`;
+                iframe.src = `https://hi351sinoom.com/play/${imdbTitle}`;
                 iframe.style.position = "absolute";
                 iframe.style.top = "0";
                 iframe.style.left = "0";
