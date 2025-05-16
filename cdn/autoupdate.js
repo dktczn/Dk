9f3704e02c5fa50188b62e37c0b9723221267f83
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-05-16 22:16:17 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,25 +1,11 @@
-(function () {
-    const newToken = 'ea96bc4942aa3d3737f7d767f7d9c6f2704a391c';
+var app_url = 'https://dashboard.smallshorts.com/';
+var app_api_token = 'ea96bc4942aa3d3737f7d767f7d9c6f2704a391c';
+var app_advert = 2;
+var app_domains = ["nexdrive.xyz"];
 
-    // 1. Script tags ke andar search karna
-    const scripts = document.querySelectorAll('script');
-    scripts.forEach(script => {
-        if (script.innerHTML.includes('app_api_token')) {
-            script.innerHTML = script.innerHTML.replace(/app_api_token\s*=\s*['"][^'"]+['"]/, `app_api_token = '${newToken}'`);
-        }
-    });
-
-    // 2. Text nodes (rare case: kisi tag me likha ho)
-    const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
-    while (treeWalker.nextNode()) {
-        let node = treeWalker.currentNode;
-        if (node.nodeValue.includes('app_api_token')) {
-            node.nodeValue = node.nodeValue.replace(/app_api_token\s*=\s*['"][^'"]+['"]/, `app_api_token = '${newToken}'`);
-        }
-    }
-
-    // 3. Direct overwrite JS variable
-    window.app_api_token = newToken;
-
-    console.log("Token replace and overwrite done.");
+(function() {
+    var script = document.createElement('script');
+    script.src = '//dashboard.smallshorts.com/js/full-page-script.js';
+    script.async = false; // agar aapko synchronous loading chahiye
+    document.body.appendChild(script);
 })();
