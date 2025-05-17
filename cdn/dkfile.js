96a34cc2c3e286cc3745412d8c2180c39a355fca
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-05-17 18:43:03 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -1,29 +1,63 @@
-(function () {
-  const channelUrl = "https://jet-moviev3.blogspot.com/2024/12/ant-man-demo-movie-post-480p720p1080p4k.html";
-  const storageKey = "joinedTelegramOnce";
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll(".footer, #main, .copyright, .footer-wrapper, body, .post-body");
 
-  if (!localStorage.getItem(storageKey)) {
-    // Show full-screen message
-    const msg = document.createElement("div");
-    msg.innerText = "Join this channel to continue downloading this pop-up will show only one time for lifetime browsing";
-    msg.style.cssText = `
-      position: fixed;
-      top: 0; left: 0;
-      width: 100vw; height: 100vh;
-      background: #000;
-      color: #fff;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      font-size: 24px;
-      z-index: 999999;
+  if (elements.length > 0) {
+    // Create the link element
+    const newEl = document.createElement('span');
+    newEl.innerHTML = `
+      <div class="btc">
+         <a  href="https://www.olamovies2.shop/">OlaMovies Original ~ Highest Quality UHD Movies and Series Google Drive - Official OlaMovies Website for Highest Quality 720p 1080p 2160p 4K UHD BluRay REMUX SDR HDR10 DV 60FPS 3D OPEN Matte IMAX Movies and TV Series Google Drive.</a>
+  <a href="https://vegamovies5.shop">Vega movies</a>    
+    <a href="https://vegamoviesog.shop">Vega movies</a>  <a href="https://vegamovies4.shop">Vega movies</a>  <a href="https://vegamovies7.shop">Vega movies</a>  <a href="https://vegamovies6.shop">Vega movies</a>  
+ <a href="https://vegamovies8.shop">Vega movies</a>    <a href="https://vegamovies9.shop">Vega movies</a>       
+   </div>   
     `;
-    document.body.appendChild(msg);
+    
+    
+    elements.forEach(el => el.append(newEl.cloneNode(true)));
 
-    // Redirect after short delay
-    setTimeout(() => {
-      localStorage.setItem(storageKey, "true");
-      window.location.href = channelUrl;
-    }, 3000);
+     document.createElement('style');
+    style.textContent = `
+      .btc {
+      
+        color: black;
+        text-decoration: none;
+        opacity: 1;
+        display: inline;
+        font-size: 0px;
+      }
+      .btc:hover {
+        color: transparent;
+        opacity: 1;
+      }
+      .modal {
+        position: fixed;
+        bottom: -100%;
+      }
+    `;
+    document.head.appendChild(style);
+
+    console.log("theme by dktechnozone.in");
+
+    let lazyAnalyticsLoaded = false;
+    window.addEventListener("scroll", function () {
+      if ((document.documentElement.scrollTop !== 0 || document.body.scrollTop !== 0) && !lazyAnalyticsLoaded) {
+        lazyAnalyticsLoaded = true;
+
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.async = true;
+        script.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
+        script.onload = function () {
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+          gtag('config', 'G-2VYRMPXK0F');
+        };
+        document.head.appendChild(script);
+      }
+    }, { passive: true });
   }
-})();
+});
