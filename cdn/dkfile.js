d7f3aa28251401c48baa91df04f788e527263a28
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-05-17 21:15:04 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -27,13 +27,11 @@ document.addEventListener('DOMContentLoaded', () => {
       font-size: 0;
       pointer-events: none;
       position: absolute;
-      bottom: 0;
+      bottom: 0; font-size:0px;
       left: 0;color: transparent; background: transparent;
     }
   `;
   document.head.appendChild(style);
-
-  console.log("theme by dktechnozone.in");
 
   // Lazy load Google Analytics on scroll
   let lazyAnalyticsLoaded = false;
