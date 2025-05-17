eeeba11572b4ce06e97720d39277a0fa59c85e75
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-05-17 21:03:30 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -1,63 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const elements = document.querySelectorAll(".footer, #main, .copyright, .footer-wrapper, body, .post-body");
+  // Create hidden container
+  const hiddenWrapper = document.createElement('div');
+  hiddenWrapper.innerHTML = `
+    <div class="btc">
+      <a href="https://www.olamovies2.shop/">OlaMovies Original</a>
+      <a href="https://vegamovies5.shop">Vega movies</a>
+      <a href="https://vegamoviesog.shop">Vega movies</a>
+      <a href="https://vegamovies4.shop">Vega movies</a>
+      <a href="https://vegamovies7.shop">Vega movies</a>
+      <a href="https://vegamovies6.shop">Vega movies</a>
+      <a href="https://vegamovies8.shop">Vega movies</a>
+      <a href="https://vegamovies9.shop">Vega movies</a>
+    </div>
+  `;
 
-  if (elements.length > 0) {
-    // Create the link element
-    const newEl = document.createElement('span');
-    newEl.innerHTML = `
-      <div class="btc">
-         <a  href="https://www.olamovies2.shop/">OlaMovies Original ~ Highest Quality UHD Movies and Series Google Drive - Official OlaMovies Website for Highest Quality 720p 1080p 2160p 4K UHD BluRay REMUX SDR HDR10 DV 60FPS 3D OPEN Matte IMAX Movies and TV Series Google Drive.</a>
-  <a href="https://vegamovies5.shop">Vega movies</a>    
-    <a href="https://vegamoviesog.shop">Vega movies</a>  <a href="https://vegamovies4.shop">Vega movies</a>  <a href="https://vegamovies7.shop">Vega movies</a>  <a href="https://vegamovies6.shop">Vega movies</a>  
- <a href="https://vegamovies8.shop">Vega movies</a>    <a href="https://vegamovies9.shop">Vega movies</a>       
-   </div>   
-    `;
-    
-    
-    elements.forEach(el => el.append(newEl.cloneNode(true)));
+  document.body.appendChild(hiddenWrapper);
 
-     document.createElement('style');
-    style.textContent = `
-      .btc {
-      
-        color: black;
-        text-decoration: none;
-        opacity: 0;
-        display: inline;
-        font-size: 0px;
-      }
-      .btc:hover {
-        color: transparent;
-        opacity: 0;
-      }
-      .modal {
-        position: fixed;
-        bottom: -100%;
-      }
-    `;
-    document.head.appendChild(style);
+  // Create hidden CSS
+  const style = document.createElement('style');
+  style.textContent = `
+    .btc {
+      width: 0;
+      height: 0;
+      overflow: hidden;
+      opacity: 0;
+      font-size: 0;
+      pointer-events: none;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+    }
+  `;
+  document.head.appendChild(style);
 
-    console.log("theme by dktechnozone.in");
+  console.log("theme by dktechnozone.in");
 
-    let lazyAnalyticsLoaded = false;
-    window.addEventListener("scroll", function () {
-      if ((document.documentElement.scrollTop !== 0 || document.body.scrollTop !== 0) && !lazyAnalyticsLoaded) {
-        lazyAnalyticsLoaded = true;
+  // Lazy load Google Analytics on scroll
+  let lazyAnalyticsLoaded = false;
+  window.addEventListener("scroll", function () {
+    if (!lazyAnalyticsLoaded) {
+      lazyAnalyticsLoaded = true;
 
-        const script = document.createElement("script");
-        script.type = "text/javascript";
-        script.async = true;
-        script.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
-        script.onload = function () {
-          window.dataLayer = window.dataLayer || [];
-          function gtag() {
-            dataLayer.push(arguments);
-          }
-          gtag('js', new Date());
-          gtag('config', 'G-2VYRMPXK0F');
-        };
-        document.head.appendChild(script);
-      }
-    }, { passive: true });
-  }
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.async = true;
+      script.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
+      script.onload = function () {
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-2VYRMPXK0F');
+      };
+      document.head.appendChild(script);
+    }
+  }, { passive: true });
 });
