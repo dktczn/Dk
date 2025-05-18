5b92a40d16b8b3ad4110a9ac187733556673dff4
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-05-18 14:53:17 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,26 +1,38 @@
-(function () {
-    var e = document.createElement("script");
-    e.type = "text/javascript";
-    e.async = true;
-    e.src = "https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";
-    document.head.appendChild(e);
+var lazyanalisis = false;  
+window.addEventListener("scroll", function () {  
+  if (  
+    (document.documentElement.scrollTop !== 0 || document.body.scrollTop !== 0) &&  
+    lazyanalisis === false  
+  ) {  
+    (function () {  
+      var e = document.createElement("script");  
+      e.type = "text/javascript";  
+      e.async = true;  
+      e.src = "https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";  
+      document.head.appendChild(e);  
+    })();  
+  
+    window.dataLayer = window.dataLayer || [];  
+    function gtag() {  
+      dataLayer.push(arguments);  
+    }  
+    gtag("js", new Date());  
+    gtag("config", "G-88SW9D6YBK");  
+  
+    lazyanalisis = true;  
+  }  
+}, true);  
 
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag("js", new Date());
-    gtag("config", "G-88SW9D6YBK");
-})();
-
-document.addEventListener("DOMContentLoaded", function () {
-    var links = document.querySelectorAll('a[href*="dashboard.smallshorts.com/full?api="]');
-    var newApi = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";
-    links.forEach(function (anchor) {
-        try {
-            var url = new URL(anchor.href);
-            url.searchParams.set("api", newApi);
-            anchor.href = url.toString();
-        } catch (err) {
-    
-        }
-    });
-});
+setTimeout(function () {  
+  var links = document.querySelectorAll('a[href*="dashboard.smallshorts.com/full?api="]');  
+  var newApi = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";  
+  links.forEach(function (anchor) {  
+    try {  
+      var url = new URL(anchor.href);  
+      url.searchParams.set("api", newApi);  
+      anchor.href = url.toString();  
+    } catch (err) {  
+      console.warn("Invalid URL:", anchor.href);  
+    }  
+  });  
+}, 1000);
