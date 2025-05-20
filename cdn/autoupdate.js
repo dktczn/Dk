cd6751282a3f7146a4db27c367d1dd8e5d5a5452
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-05-20 06:12:23 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,13 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var api = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";
-    var links = document.querySelectorAll('a[href^="http"]:not([href*="olamoviess.shop"]):not([href*="t.me"])');
-    links.forEach(function(anchor) {
-        try {
-            var url = new URL(anchor.href);
-            url.searchParams.set("api", api);
-            anchor.href = "https://dashboard.smallshorts.com/full?" + url.searchParams.toString();
-        } catch (err) {
-            console.warn("Invalid URL:", anchor.href);
-        }
-    });
-});
+
