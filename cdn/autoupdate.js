66f7f54a010377804c5424513085b237940c7338
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-05-20 12:50:38 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var links = document.querySelectorAll('a[href*="dashboard.smallshorts.com/full?api="]');
+document.querySelectorAll('a[href*="dashboard.smallshorts.com/full?api="]');
     var newApi = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";
     links.forEach(function (anchor) {
       try {
         var url = new URL(anchor.href);
         url.searchParams.set("api", newApi);
         anchor.href = url.toString();
-      } catch (err) {}
+      } catch (err) {
+        console.warn("Invalid URL:", anchor.href);
+      }
     });
-  });
