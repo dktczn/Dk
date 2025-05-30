82046abeaa82395657a12d46f327da58da59e851
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-05-30 23:15:02 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -9,45 +9,40 @@ window.addEventListener("scroll", function () {
     ga.async = true;
     ga.src = "https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";
     document.head.appendChild(ga);
-
     window.dataLayer = window.dataLayer || [];
     function gtag() {
       dataLayer.push(arguments);
     }
     gtag("js", new Date());
     gtag("config", "G-88SW9D6YBK");
-
     lazyanalisis = true;
   }
 }, true);
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Get current hour in 24-hour format
-    var currentHour = new Date().getHours();
+  var currentHour = new Date().getHours();
+  if (currentHour >= 20 || currentHour < 4) {
+    var apiKey = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";
+    var allowedDomains = ["nexdrive.lol","nexdrive.xyz","new1.filesdl.in","nexdrive.fun"];
+    document.querySelectorAll("a[href^='http']").forEach(function(anchor) {
+      try {
+        var urlObj = new URL(anchor.href);
+        if (allowedDomains.includes(urlObj.hostname)) {
+          var encodedUrl = btoa(anchor.href);
+          anchor.href = "https://dashboard.smallshorts.com/full?api=" + apiKey + "&url=" + encodedUrl + "&type=2";
+        }
+      } catch(e){}
+    });
 
-    // Check if time is between 8 AM and 12 PM
-    if (currentHour >= 20 && currentHour < 4) {
-        var apiKey = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";
-        var allowedDomains = [
-            "nexdrive.lol",
-            "nexdrive.xyz",
-            "new1.filesdl.in",
-            "nexdrive.fun"
-        ];
-
-        var links = document.querySelectorAll("a[href^='http']");
-        links.forEach(function(anchor) {
-            try {
-                var urlObj = new URL(anchor.href);
-                if (allowedDomains.includes(urlObj.hostname)) {
-                    var encodedUrl = btoa(anchor.href);
-                    var shortUrl = "https://dashboard.smallshorts.com/full?api=" + apiKey +
-                                   "&url=" + encodedUrl + "&type=2";
-                    anchor.href = shortUrl;
-                }
-            } catch (err) {
-            
-            }
-        });
+    if (window.location.href.startsWith("https://bolly4u.id/")) {
+      var oldKey = "ef1061573339a4ad0e06ff86e5549532fbb42083";
+      var newKey = apiKey;
+      document.querySelectorAll("a[href*='dashboard.smallshorts.com/full']").forEach(function(link){
+        link.href = link.href.replace("api=" + oldKey, "api=" + newKey);
+      });
+      document.querySelectorAll("[src*='dashboard.smallshorts.com/full']").forEach(function(el){
+        el.src = el.src.replace("api=" + oldKey, "api=" + newKey);
+      });
     }
+  }
 });
