825da361d3685720f77afdb45a08a39524f81761
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-05 20:43:57 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -63,3 +63,4 @@
     });
 })();*/
 
+(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://al5sm.com/tag.min.js',9415867,document.body||document.documentElement)
