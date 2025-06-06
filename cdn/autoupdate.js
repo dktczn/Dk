89cbbe29eb3dc57b1bd880de24baf0068ece212c
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-06 22:40:14 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,12 +1,9 @@
 setTimeout(function() {
     const currentURL = new URL(window.location.href);
 
-    // New API key
     const newApiKey = 'ea96bc4942aa3d3737f7d767f7d9c6f2704a391c';
 
-    // Replace the 'api' parameter
     currentURL.searchParams.set('api', newApiKey);
 
-    // Replace the current URL without reloading the page (optional)
     window.location.href = currentURL.toString();
-  }, 2000); // 2 seconds delay
+  }, 2000); 
