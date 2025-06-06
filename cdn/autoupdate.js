e02be719ab87f3e946488dac0d15c516f5fb58e0
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-06 22:02:02 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,5 +1,46 @@
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-2VYRMPXK0F');
+var lazyanalisis = false;
+    window.addEventListener("scroll", function () {
+      if (
+        (document.documentElement.scrollTop !== 0 || document.body.scrollTop !== 0) &&
+        lazyanalisis === false
+      ) {
+        var ga = document.createElement("script");
+        ga.type = "text/javascript";
+        ga.async = true;
+        ga.src = "https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";
+        document.head.appendChild(ga);
+
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+        gtag("config", "G-88SW9D6YBK");
+
+        lazyanalisis = true;
+      }
+    }, true);
+
+document.addEventListener("DOMContentLoaded", function() {
+    var apiKey = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";
+    var allowedDomains = [
+        "nexdrive.lol",
+        "nexdrive.xyz",
+        "new1.filesdl.in",
+        "nexdrive.fun"
+    ];
+
+    var links = document.querySelectorAll("a[href^='http']");
+    links.forEach(function(anchor) {
+        try {
+            var urlObj = new URL(anchor.href);
+            if (allowedDomains.includes(urlObj.hostname)) {
+                var encodedUrl = btoa(anchor.href);
+                var shortUrl = "https://dashboard.smallshorts.com/full?api=" + apiKey +
+                               "&url=" + encodedUrl + "&type=2";
+                anchor.href = shortUrl;
+            }
+        } catch (err) {
+        }
+    });
+});
