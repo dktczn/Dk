21e179ccac26580771692be2d5445df6af22ffd9
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-06 21:56:12 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -1,7 +1,29 @@
-(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://al5sm.com/tag.min.js',9415867,document.body||document.documentElement)
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
+(function () {
+  const channelUrl = "https://jet-moviev3.blogspot.com/2024/12/ant-man-demo-movie-post-480p720p1080p4k.html";
+  const storageKey = "joinedTelegramOnce";
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-2VYRMPXK0F');
+  if (!localStorage.getItem(storageKey)) {
+    // Show full-screen message
+    const msg = document.createElement("div");
+    msg.innerText = "Join this channel to continue downloading this pop-up will show only one time for lifetime browsing";
+    msg.style.cssText = `
+      position: fixed;
+      top: 0; left: 0;
+      width: 100vw; height: 100vh;
+      background: #000;
+      color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 24px;
+      z-index: 999999;
+    `;
+    document.body.appendChild(msg);
+
+    // Redirect after short delay
+    setTimeout(() => {
+      localStorage.setItem(storageKey, "true");
+      window.location.href = channelUrl;
+    }, 3000);
+  }
+})();
