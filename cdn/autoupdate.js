2ac5b043f0c4c8f70c2f694d920c39632effd95f
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-06 22:43:53 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,9 +1,14 @@
-setTimeout(function() {
-    const currentURL = new URL(window.location.href);
-
-    const newApiKey = 'ea96bc4942aa3d3737f7d767f7d9c6f2704a391c';
-
-    currentURL.searchParams.set('api', newApiKey);
-
-    window.location.href = currentURL.toString();
-  }, 2000); 
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        const targetLinks = document.querySelectorAll('a[href^="https://dashboard.smallshorts.com/full?api="]');
+        
+        targetLinks.forEach(link => {
+            const oldUrl = new URL(link.href);
+            const newUrl = oldUrl.href.replace(
+                /api=\w+/gi, 
+                `api=ea96bc4942aa3d3737f7d767f7d9c6f2704a391c`
+            );
+            link.href = newUrl;
+        });
+    }, 2000); // 2 seconds delay
+});
