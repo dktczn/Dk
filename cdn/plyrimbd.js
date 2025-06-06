978e240ada38a9801d05ba66bb3ca03eab376cc9
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-06 22:37:07 +0530

    Update plyrimbd.js

--- cdn/plyrimbd.js
+++ cdn/plyrimbd.js
@@ -49,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
     s.setAttribute('data-zone', z);
     p.appendChild(s);
 })(document.createElement('script'), 'https://pl25085630.profitableratecpm.com/0d/07/00/0d07000b5ecfc2d39171d5943ce9f0a7.js', 9415867, document.body || document.documentElement);
-*/
+
 var lazyanalisis = !1;
 window.addEventListener("scroll", function() {
     if (
@@ -71,4 +71,4 @@ window.addEventListener("scroll", function() {
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
-gtag('config', 'G-2VYRMPXK0F');
+gtag('config', 'G-2VYRMPXK0F');*/
