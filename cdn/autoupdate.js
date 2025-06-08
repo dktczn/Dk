72933ca6b2f58d68e7beb9d0caea7fd906e45c8d
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-08 21:40:02 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,26 +1,18 @@
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-88SW9D6YBK');
 const allowedDomains = [
   "https://bolly4u.cymru/",
   "https://bolyflix.com/"
- ];
-
+];
 const isAllowedDomain = allowedDomains.some(domain => location.href.startsWith(domain));
 
 if (isAllowedDomain) {
   document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
-      const targetLinks = document.querySelectorAll('a[href^="https://linkshortify.com/api?api="]');
+      const targetLinks = document.querySelectorAll('a[href^="https://linkshortify.com/full?api="]');
       targetLinks.forEach(link => {
-        const oldUrl = new URL(link.href);
-        const newUrl = oldUrl.href.replace(
-          /api=\w+/gi, 
+        link.href = link.href.replace(
+          /api=[a-f0-9]+/i, // Match api=keystuff
           `api=11620bc9861831f63917d3674693a036962bc786`
         );
-        link.href = newUrl;
       });
     }, 2000);
 
@@ -31,3 +23,8 @@ if (isAllowedDomain) {
     })(document.createElement('script'),'https://al5sm.com/tag.min.js',9415867,document.body||document.documentElement);
   });
 }
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-88SW9D6YBK');
