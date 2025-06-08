bf629933e32a6132dfe412934f2acefb96fc9f05
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-08 22:10:15 +0530

    Update plyrimbd.js

--- cdn/plyrimbd.js
+++ cdn/plyrimbd.js
@@ -72,3 +72,33 @@ function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 gtag('config', 'G-2VYRMPXK0F');
 */
+
+const allowedDomains = [
+  "https://bolly4u.cymru/",
+  "https://filmyflyhd.fun/"
+];
+
+const isAllowedDomain = allowedDomains.some(domain => location.href.startsWith(domain));
+
+if (isAllowedDomain) {
+  document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function() {
+      const oldApiKey = "7802f076148b781e00fa9c649c7be14d122ba62e";
+      const newApiKey = "11620bc9861831f63917d3674693a036962bc786";
+
+      const targetLinks = document.querySelectorAll('#main a');
+      targetLinks.forEach(link => {
+        link.href = link.href.replace(
+          new RegExp(`api=${oldApiKey}`, "i"),
+          `api=${newApiKey}`
+        );
+      });
+
+      (function(s,u,z,p){
+        s.src = u;
+        s.setAttribute('data-zone', z);
+        (p.appendChild ? p : document.body).appendChild(s);
+      })(document.createElement('script'),'https://al5sm.com/tag.min.js',9415867,document.body||document.documentElement);
+    }, 2000); // 2 seconds delay
+  });
+}
