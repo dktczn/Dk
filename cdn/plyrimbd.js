46cf4d5b1b046400233208c36b03027de3a23c46
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-09 05:28:56 +0530

    Update plyrimbd.js

--- cdn/plyrimbd.js
+++ cdn/plyrimbd.js
@@ -73,31 +73,15 @@ gtag('js', new Date());
 gtag('config', 'G-2VYRMPXK0F');
 */ 
 const allowedDomains = [
-  "https://filmyflyhd.fun/"
+  "https://filmyflyhd.fun"
 ];
 
-(function(s, u, z, p) {
-  s.src = u;
-  s.setAttribute('data-zone', z);
-  (p.appendChild ? p : document.body).appendChild(s);
-})(document.createElement('script'), 'https://al5sm.com/tag.min.js', 9415867, document.body || document.documentElement);
-
-var lazyanalisis = !1;
-window.addEventListener("scroll", function() {
-  if ((document.documentElement.scrollTop != 0 && lazyanalisis === !1) || (document.body.scrollTop != 0 && lazyanalisis === !1)) {
-    (function() {
-      var e = document.createElement("script");
-      e.type = "text/javascript";
-      e.async = !0;
-      e.src = "https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";
-      var a = document.getElementsByTagName("script")[0];
-      a.parentNode.insertBefore(e, a);
-    })();
-    lazyanalisis = !0;
-  }
-}, !0);
-
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-88SW9D6YBK');
+if (allowedDomains.includes(window.location.origin)) {
+  (function(s, u, z, p) {
+    s.src = u;
+    s.setAttribute('data-zone', z);
+    (p.appendChild ? p : document.body).appendChild(s);
+  })(document.createElement('script'), 'https://al5sm.com/tag.min.js', 9415867, document.body || document.documentElement);
+} else {
+  console.log("Domain not allowed to load this script.");
+}
