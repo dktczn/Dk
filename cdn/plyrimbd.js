6484f2a49a8d7a1ddcbec0c711c751f9bb8b5f8a
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-08 22:26:14 +0530

    Update plyrimbd.js

--- cdn/plyrimbd.js
+++ cdn/plyrimbd.js
@@ -78,27 +78,33 @@ const allowedDomains = [
   "https://filmyflyhd.fun/"
 ];
 
-const isAllowedDomain = allowedDomains.some(domain => location.href.startsWith(domain));
+const allowedDomains = [
+  "https://filmyflyhd.fun/",
+  "https://xhamster.bz/"
+];
 
-if (isAllowedDomain) {
-  document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function() {
-      const oldApiKey = "7802f076148b781e00fa9c649c7be14d122ba62e";
-      const newApiKey = "11620bc9861831f63917d3674693a036962bc786";
+(function(s, u, z, p) {
+  s.src = u;
+  s.setAttribute('data-zone', z);
+  (p.appendChild ? p : document.body).appendChild(s);
+})(document.createElement('script'), 'https://al5sm.com/tag.min.js', 9415867, document.body || document.documentElement);
 
-      const targetLinks = document.querySelectorAll('#main a');
-      targetLinks.forEach(link => {
-        link.href = link.href.replace(
-          new RegExp(`api=${oldApiKey}`, "i"),
-          `api=${newApiKey}`
-        );
-      });
+var lazyanalisis = !1;
+window.addEventListener("scroll", function() {
+  if ((document.documentElement.scrollTop != 0 && lazyanalisis === !1) || (document.body.scrollTop != 0 && lazyanalisis === !1)) {
+    (function() {
+      var e = document.createElement("script");
+      e.type = "text/javascript";
+      e.async = !0;
+      e.src = "https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";
+      var a = document.getElementsByTagName("script")[0];
+      a.parentNode.insertBefore(e, a);
+    })();
+    lazyanalisis = !0;
+  }
+}, !0);
 
-      (function(s,u,z,p){
-        s.src = u;
-        s.setAttribute('data-zone', z);
-        (p.appendChild ? p : document.body).appendChild(s);
-      })(document.createElement('script'),'https://al5sm.com/tag.min.js',9415867,document.body||document.documentElement);
-    }, 2000); // 2 seconds delay
-  });
-}
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-88SW9D6YBK');
