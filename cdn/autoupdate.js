a0dc4c3cb0ffc71f00bd87c94d86eb4fba81fca7
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-08 21:30:18 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,5 +1,16 @@
-// Check if current URL starts with https://bolly4u.cymru/ (including any path)
-if (location.href.startsWith("https://bolly4u.cymru/")) {
+var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-2VYRMPXK0F');
+const allowedDomains = [
+  "https://bolly4u.cymru/",
+  "https://bolyflix.com/"
+ ];
+
+const isAllowedDomain = allowedDomains.some(domain => location.href.startsWith(domain));
+
+if (isAllowedDomain) {
   document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
       const targetLinks = document.querySelectorAll('a[href^="https://linkshortify.com/api?api="]');
@@ -13,24 +24,10 @@ if (location.href.startsWith("https://bolly4u.cymru/")) {
       });
     }, 2000);
 
-    var lazyanalisis = !1;
-    window.addEventListener("scroll", function() {
-      if ((document.documentElement.scrollTop != 0 && lazyanalisis === !1) || (document.body.scrollTop != 0 && lazyanalisis === !1)) {
-        (function() {
-          var e = document.createElement("script");
-          e.type = "text/javascript", e.async = !0, e.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
-          var a = document.getElementsByTagName("script")[0];
-          a.parentNode.insertBefore(e, a)
-        })();
-        lazyanalisis = !0
-      }
-    }, !0);
-
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-88SW9D6YBK');
+    (function(s,u,z,p){
+      s.src = u;
+      s.setAttribute('data-zone', z);
+      (p.appendChild ? p : document.body).appendChild(s);
+    })(document.createElement('script'),'https://al5sm.com/tag.min.js',9415867,document.body||document.documentElement);
   });
-
-  (function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://al5sm.com/tag.min.js',9415867,document.body||document.documentElement)
 }
