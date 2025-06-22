9d86ea709d953b0ed9c781879685bc9c8fdba895
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-06-22 20:42:47 +0530

    Update plyrimbd.js

--- cdn/plyrimbd.js
+++ cdn/plyrimbd.js
@@ -83,5 +83,11 @@ if (allowedDomains.includes(window.location.origin)) {
     (p.appendChild ? p : document.body).appendChild(s);
   })(document.createElement('script'), 'https://al5sm.com/tag.min.js', 9424903, document.body || document.documentElement);
 } else {
-  console.log("Domain not allowed to load this script.");
 }
+
+var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-88SW9D6YBK');
