3a93d4aa44e2aba63e3f5db1e0aa5040199fe86f
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-07-04 08:12:24 +0530

    Update plyrimbd.js

--- cdn/plyrimbd.js
+++ cdn/plyrimbd.js
@@ -72,7 +72,7 @@ function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 gtag('config', 'G-2VYRMPXK0F');
 */ 
-const allowedDomains = [
+/*const allowedDomains = [
   "https://filmyflyhd.fun"
 ];
 
@@ -84,7 +84,7 @@ if (allowedDomains.includes(window.location.origin)) {
   })(document.createElement('script'), 'https://al5sm.com/tag.min.js', 9479383, document.body || document.documentElement);
 } else {
 }
-
+*/
 var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
 
   window.dataLayer = window.dataLayer || [];
