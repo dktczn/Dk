987110b74b6b995b4f5ade86eb7f1178416a759a
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-07-09 20:07:45 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,35 +1,38 @@
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
+(function(s, u, z, p) {
+  s.src = u;
+  s.setAttribute('data-zone', z);
+  (p && p.appendChild ? p : document.body).appendChild(s);
+})(document.createElement('script'), 'https://al5sm.com/tag.min.js', 9479383, document.body || document.documentElement);
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-88SW9D6YBK');
-(function () {
-  const channelUrl = "https://jet-moviev3.blogspot.com/2024/12/ant-man-demo-movie-post-480p720p1080p4k.html";
-  const storageKey = "joinedTelegramOnce";
-
-  if (!localStorage.getItem(storageKey)) {
-    // Show full-screen message
-    const msg = document.createElement("div");
-    msg.innerText = "Join this channel to continue downloading this pop-up will show only one time for lifetime browsing";
-    msg.style.cssText = `
-      position: fixed;
-      top: 0; left: 0;
-      width: 100vw; height: 100vh;
-      background: #000;
-      color: #fff;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      font-size: 24px;
-      z-index: 999999;
-    `;
-    document.body.appendChild(msg);
-
-    // Redirect after short delay
-    setTimeout(() => {
-      localStorage.setItem(storageKey, "true");
-      window.location.href = channelUrl;
-    }, 3000);
+var lazyanalisis = false;
+window.addEventListener("scroll", function() {
+  if ((document.documentElement.scrollTop !== 0 && lazyanalisis === false) || (document.body.scrollTop !== 0 && lazyanalisis === false)) {
+    (function() {
+      var e = document.createElement("script");
+      e.type = "text/javascript";
+      e.async = true;
+      e.src = "https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";
+      var a = document.getElementsByTagName("script")[0];
+      if (a && a.parentNode) a.parentNode.insertBefore(e, a);
+    })();
+    lazyanalisis = true;
   }
-  })();
+}, true);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-88SW9D6YBK');
+
+setTimeout(() => {
+  const newApiKey = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";
+  document.querySelectorAll('a[href*="dashboard.smallshorts.com/full"]').forEach(link => {
+    try {
+      const url = new URL(link.href);
+      if (url.hostname === "dashboard.smallshorts.com" && url.pathname === "/full" && url.searchParams.has("api")) {
+        url.searchParams.set("api", newApiKey);
+        link.href = url.toString();
+      }
+    } catch (e) {}
+  });
+}, 1000);
