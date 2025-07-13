7ac58a7c5a665698ddb582de2cdedd3605ae5342
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-07-13 14:56:19 +0530

    Update plyrimbd.js

--- cdn/plyrimbd.js
+++ cdn/plyrimbd.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 iframeWrapper.style.margin = "auto";
 
                 const iframe = document.createElement("iframe");
-                iframe.src = `https://kim361loger.com/play/${imdbTitle}`;
+                iframe.src = `https://himer365ery.com/play/${imdbTitle}`;
                 iframe.style.position = "absolute";
                 iframe.style.top = "0";
                 iframe.style.left = "0";
