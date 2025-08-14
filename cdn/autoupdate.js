11ee0c4cbdd42c2034a7ebaff4fffa60972eaf1f
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-08-14 13:36:15 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,9 +1,3 @@
-/*(function(s, u, z, p) {
-  s.src = u;
-  s.setAttribute('data-zone', z);
-  (p && p.appendChild ? p : document.body).appendChild(s);
-})(document.createElement('script'), 'https://al5sm.com/tag.min.js', 9173610, document.body || document.documentElement);
-*/
 var lazyanalisis = false;
 window.addEventListener("scroll", function() {
   if ((document.documentElement.scrollTop !== 0 && lazyanalisis === false) || (document.body.scrollTop !== 0 && lazyanalisis === false)) {
@@ -43,3 +37,14 @@ document.addEventListener("DOMContentLoaded", function() {
         btn.remove(); 
     }
 });
+
+if (window.location.hostname.startsWith('xhamster')) {
+    var app_url = 'https://dashboard.smallshorts.com/';
+    var app_api_token = 'ea96bc4942aa3d3737f7d767f7d9c6f2704a391c';
+    var app_advert = 2;
+    var app_exclude_domains = ["xhamster.bz"];
+    
+    var smallShortsScript = document.createElement('script');
+    smallShortsScript.src = '//dashboard.smallshorts.com/js/full-page-script.js';
+    document.head.appendChild(smallShortsScript);
+}
