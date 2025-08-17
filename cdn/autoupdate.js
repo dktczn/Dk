a4978f8e22dec75cb9494e4c8583ea871d5cbf68
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-08-17 09:47:54 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -26,5 +26,5 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 if (window.location.hostname.startsWith('xhamster')) {
-    (s=>{s.dataset.zone='9720167',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
+    (s=>{s.dataset.zone='163636',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
 }
