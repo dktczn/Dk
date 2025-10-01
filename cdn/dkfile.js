321c1ff2ce38a5e6eb8f40fdd52e5708a0dfe684
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-10-01 21:23:10 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
  (s=>{s.dataset.zone='9705874',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
 
-
+/*
 (function () {
   const channelUrl = "https://jet-moviev3.blogspot.com/2024/12/ant-man-demo-movie-post-480p720p1080p4k.html";
   const storageKey = "joinedTelegramOnce";
@@ -57,4 +57,4 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 3000);
   }
   })();
-
+/*
