939a9efc0b26cad4a449e1ec4e5ac7bbb83f0f9e
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-11-19 07:05:17 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,47 +1,6 @@
-var lazyanalisis = false;
-
-function isAllowedTime() {
-  var now = new Date();
-  var hour = now.getHours();
-  return (hour >= 20 || hour < 5); 
-}
-
-if (isAllowedTime()) {
-  window.addEventListener("scroll", function() {
-    if ((document.documentElement.scrollTop !== 0 && lazyanalisis === false) || 
-        (document.body.scrollTop !== 0 && lazyanalisis === false)) {
-      (function() {
-        var e = document.createElement("script");
-        e.type = "text/javascript";
-        e.async = true;
-        e.src = "https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";
-        var a = document.getElementsByTagName("script")[0];
-        if (a && a.parentNode) a.parentNode.insertBefore(e, a);
-      })();
-      lazyanalisis = true;
-    }
-  }, true);
+var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-88SW9D6YBK";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
 
   window.dataLayer = window.dataLayer || [];
-  function gtag() { dataLayer.push(arguments); }
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'G-88SW9D6YBK');
-
-  document.addEventListener("DOMContentLoaded", function() {
-    var btn = document.getElementById("v0-built-with-button-fd4bdc15-41d2-4d5b-8d92-350d5612846b");
-    if (btn) {
-      btn.remove(); 
-    }
-  });
-
-  if (window.location.hostname.startsWith('xhamster')) {
-    (s=>{
-      s.dataset.zone='10059814';
-      s.src='https://al5sm.com/tag.min.js';
-    })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
-  }
-} else {
-  }
-document.querySelectorAll('script[src*="al5sm.com/tag.min.js"]').forEach(el => {
-  el.setAttribute('data-zone', '9424903');
-});
