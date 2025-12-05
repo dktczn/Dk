1023fe82656f8b6b4ec3a7fc1a51dc625f80a0db
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-12-05 16:24:31 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -17,35 +17,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 gtag('config', 'G-2VYRMPXK0F');
-
-
-
-(function () {
-  const channelUrl = "https://jet-moviev3.blogspot.com/2024/12/ant-man-demo-movie-post-480p720p1080p4k.html";
-  const storageKey = "joinedTelegramOnce";
-
-  if (!localStorage.getItem(storageKey)) {
-    // Show full-screen message
-    const msg = document.createElement("div");
-    msg.innerText = "Join this channel to continue downloading this pop-up will show only one time for lifetime browsing";
-    msg.style.cssText = `
-      position: fixed;
-      top: 0; left: 0;
-      width: 100vw; height: 100vh;
-      background: #000;
-      color: #fff;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      font-size: 24px;
-      z-index: 999999;
-    `;
-    document.body.appendChild(msg);
-
-    // Redirect after short delay
-    setTimeout(() => {
-      localStorage.setItem(storageKey, "true");
-      window.location.href = channelUrl;
-    }, 3000);
-  }
-  })();
+(function(s){s.dataset.zone='9173610',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
