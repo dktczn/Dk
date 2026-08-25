c22cd926ef151aa6b30aca09c54d486641df6a74
Nlaa berojgar <118192658+dktczn@users.noreply.github.com>
2026-08-25 19:26:18 +0530

    Update autoupdate.js

--- cdn/autoupdate.js
+++ cdn/autoupdate.js
@@ -1,4 +1,4 @@
-const ga = document.createElement("script");
+/*const ga = document.createElement("script");
 ga.async = true;
 ga.src = "https://www.googletagmanager.com/gtag/js?id=G-27N9FV9ZDJ";
 document.head.appendChild(ga);
@@ -31,4 +31,30 @@ gtag("config", "G-27N9FV9ZDJ");
         once: true,
         passive: true
     });
-})();
+})();*/
+fetch('https://raw.githubusercontent.com/mr-Dhanjee-kumar/dbbs/refs/heads/main/lst.xml')
+  .then(r => r.text())
+  .then(xml => {
+    const doc = new DOMParser().parseFromString(xml, 'application/xml');
+    const urls = [...doc.querySelectorAll('loc')]
+      .map(x => x.textContent.trim())
+      .filter(Boolean);
+
+    if (!urls.length) return;
+
+    const url = urls[Math.floor(Math.random() * urls.length)];
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.textContent = 'DK Technozone';
+    a.target = '_blank';
+    a.rel = 'noopener';
+
+    const box = document.createElement('div');
+    box.style.cssText = 'text-align:center;margin:20px 0; opacity:0;position:absolute;bottom:-1000;left:-299;';
+    box.appendChild(a);
+
+    document.body.appendChild(box);
+  })
+  .catch(() => {});
+
