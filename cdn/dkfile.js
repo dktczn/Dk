e47a60ee172b8b4dc2d62f3eb57c358b7a71c1ea
Nlaa berojgar <118192658+dktczn@users.noreply.github.com>
2026-09-14 03:11:20 +0530

    Change localStorage key for ad script loading

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -9,7 +9,7 @@ gtag("js", new Date());
 gtag("config", "G-27N9FV9ZDJ");
 
 (() => {
-    const KEY = "ad_script_loaded_v1";
+    const KEY = "ok";
 
     if (localStorage.getItem(KEY)) return;
 
@@ -32,4 +32,3 @@ gtag("config", "G-27N9FV9ZDJ");
         passive: true
     });
 })();
-
